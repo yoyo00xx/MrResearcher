ec8c0910389991b6b15c57b307d8e702be648c77
Younes Balah <s201518650@kfupm.edu.sa>
2019-04-09 19:10:16 +0300

    Init

---
+++ src/MainPackage/MyProgram.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MainPackage;
+
+import java.awt.Color;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+
+/**
+ *
+ * @author BalaH-RiG
+ */
+public class MyProgram {
+    public static void main(String[] args )
+    {
+        
+        (new MainWindow()).setVisible(true);
+        
+        
+    }
+    public static void systemTheme(){
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+    }
+    public static void defaultTheme(){
+        try {          
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    public static void restoreTheme(){
+        try {
+            UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");     
+            
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(MyProgram.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
