2ada8fc2a1bd63b72cf42eeff4ec451c19f5a532
Abdullah Barnawi <ab.barnawi@gmail.com>
2019-04-11 18:19:18 +0300

    Lost

--- src/MainPackage/PapersManager.java
+++ src/MainPackage/PapersManager.java
@@ -7,7 +7,7 @@ import java.util.logging.Logger;
 
 public class PapersManager implements Serializable {
 
-    private ArrayList<Paper> papers = new ArrayList<>();
+    private static ArrayList<Paper> papers = new ArrayList<>();
 
     public static void main(String[] args) {
 
